5d51c3494d96471370be81cf24690328f62d89f9
shekh <fairdabhairakdar@gmail.com>
2020-11-30 12:26:40 +0530

    Simply Dome Spinner View

--- app/src/main/java/com/malkinfo/spinerdome/MainActivity.kt
+++ app/src/main/java/com/malkinfo/spinerdome/MainActivity.kt
@@ -2,10 +2,35 @@ package com.malkinfo.spinerdome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val customList = listOf<String>("First","Second","Third","Fourth")
+        val adapter = ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,customList)
+        spinner.adapter = adapter
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                Toast.makeText(this@MainActivity,
+                    "You can Selected ${parent?.getItemAtPosition(position)
+                        .toString()}",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
     }
 }
